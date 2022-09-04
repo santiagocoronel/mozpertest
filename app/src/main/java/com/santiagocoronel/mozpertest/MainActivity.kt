3706a999f39b9f425c436f8a019f3a8a870b8c5d
Santiago Coronel <santiago@medicato.com>
2022-09-04 13:15:28 -0300

    initial impl

---
+++ app/src/main/java/com/santiagocoronel/mozpertest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.santiagocoronel.mozpertest
+
+import com.santiagocoronel.androidbase.presenter.BaseActivity
+import com.santiagocoronel.mozpertest.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun init() {
+
+    }
+}
