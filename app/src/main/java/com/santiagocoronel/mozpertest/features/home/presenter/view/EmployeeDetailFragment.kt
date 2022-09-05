14682ac4a7db8f283fc322adab817de253291ef6
Santiago Coronel <santiago@medicato.com>
2022-09-05 07:34:47 -0300

    add fragments

---
+++ app/src/main/java/com/santiagocoronel/mozpertest/features/home/presenter/view/EmployeeDetailFragment.kt
@@ -0,0 +1,28 @@
+package com.santiagocoronel.mozpertest.features.home.presenter.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.santiagocoronel.androidbase.presenter.BaseActivity
+import com.santiagocoronel.androidbase.presenter.BaseFragment
+import com.santiagocoronel.androidbase.presenter.BaseViewModel
+import com.santiagocoronel.mozpertest.R
+import com.santiagocoronel.mozpertest.databinding.FragmentEmployeeDetailBinding
+
+
+class EmployeeDetailFragment : BaseFragment<FragmentEmployeeDetailBinding, BaseActivity<*>>() {
+
+    override fun getViewModel(): BaseViewModel? {
+        TODO("Not yet implemented")
+    }
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupObserversViewModel() {
+        TODO("Not yet implemented")
+    }
+}
